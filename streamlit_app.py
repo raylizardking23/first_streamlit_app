87fdc3424c987f92fe30e0cd79e486e6223ac553
raylizardking23 <40242588+raylizardking23@users.noreply.github.com>
2022-04-30 23:07:56 -0500

    Update streamlit_app.py
    Removing duplicated title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 
 streamlit.title('Breakfast Favorites')
-streamlit.header('Breakfast Favorites')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
