a40cf58956f114ceeaf4f7c0e2ac97c0f69e632d
raylizardking23 <40242588+raylizardking23@users.noreply.github.com>
2022-05-07 14:07:14 -0500

    Update streamlit_app.py
    Fixing comment syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-"New section to display fruityvice api response
+#New section to display fruityvice api response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
